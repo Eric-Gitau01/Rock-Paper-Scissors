df8c6e3ebab79476c7d993d96f8a9232ae124d10
eric <gitaueric01@gmail.com>
2025-03-04 12:33:48 +0300

    feat: implement Rock paper Scissors game logic add 'getComputerMove' function to generate random computer moves. implement 'rock' , 'paper', and 'scissors' function to handle  user choice and determine game out come. Display result dynamically in the DOM. FIX variable naming conflicts and improve code clarity

--- script.js
+++ script.js
@@ -1,114 +1,61 @@
-// const score = JSON.parse(localStorage.getItem('score')) || {
-//     wins: 0,
-//     losses: 0,
-//     ties: 0
-// }
-
-
-// function playGame(playerMove) {
-//     const randomMove = Math.random();
-//     let computerMove = ''
-//     let result = ''
-
-   
-//     if (randomMove >= 0 && randomMove < 1 / 3 ) {
-//         computerMove = 'rock'
-//     } else if (randomMove >= 1 / 3 && randomMove < 2 / 3 ) {
-//         computerMove = 'paper'
-//     } else {
-//         computerMove = 'scissors'
-//     }
-
-//     if (playerMove === 'rock') {
-//         if (computerMove === 'rock') {
-//             result = 'tie'
-//         } else if (computerMove === 'paper') {
-//             result = 'lose'
-//         } else if ( computerMove === 'scissors') {
-//             result = 'win'
-//         }
-//     } else if (playerMove === 'paper'){
-//         if (computerMove === 'rock') {
-//             result = 'lose'
-//         } else if (computerMove === 'paper') {
-//             result = 'tie'
-//         } else if ( computerMove === 'scissors') {
-//             result = 'win'
-//         }
-//     } else if (playerMove === 'scissior') {
-//         if (computerMove === 'rock') {
-//             result = 'win'
-//         } else if (computerMove === 'paper') {
-//             result = 'lose'
-//         } else if ( computerMove === 'scissors') {
-//             result = 'tie'
-//         }    
-//     }
-    
-//     if (result === 'win') {
-//         score.wins++
-//     } else if (result === 'lose') {
-//         score.losses++
-//     } else if ( result === 'tie') {
-//         score.tie++
-//     }
-
-//     localStorage.setItem('score', JSON.stringify(score))
-//     alert( `You chose ${playerMove} Computer chose ${computerMove}. You ${result}.
-// wins: ${score.wins}, losses: ${score.losses}, ties: ${score.tie}`)
-
-// }
-
-// function rock() {
-//    playGame('rock')
-// }
-
-
-
-
-// function paper() {
-//     playGame('paper')
-// }
-
-// function scissior() {
-//     playGame('scissior')
-// }
-
-// function resetGame() {
-//     score.wins = 0
-//     score.losses = 0
-//     score.tie = 0
-//     localStorage.removeItem('score')
-//     alert('Game reset. Wins: 0, Losses: 0, Ties: 0')
-// }
-
-
-
-   
-const showQualityBtn = document.querySelector('.showQuality-btn')
-const addItemBtn = document.querySelector('.addItem-btn')
-const add2 = document.querySelector('.+2')
-const add3 = document.querySelector('.+3-btn')
-const add4 = document.querySelector('.+4-btn')
-const add5 = document.querySelector('.+5-btn')
-const removeItemBtn = document.querySelector('.removeItem-btn')
-const substract2 = document.querySelector('.-2-btn')
-const substract3 = document.querySelector('.-3-btn')
-const resetBtn = document.querySelector('.reset-btn') 
-
-
-
-function updateCartQuality(change) {
-    if (cartQuality + change > 10) {
-        console.log('cart is full')
-    } else if (cartQuality + change < 10) {
-        console.log('cart is not empty')
+function getcomputerMove() {
+    let computerMove = '';
+    let randomNumber = Math.random();
+    if (randomNumber < 0.33) {
+      computerMove = 'rock';
+    } else if (randomNumber < 0.66) {
+      computerMove = 'paper';
     }
-
-    cartQuality += change
-    console.log(`cart quality ${cartQuality}`)
+    else {
+      computerMove = 'scissors';
+    }
+    return computerMove;
 }
 
-addItemBtn.addEventListener('click', function(){
-    updateCartQuality(1)
-})
+function rock() {
+    let computerMove = getcomputerMove();
+    let resultEl = document.getElementById('result');
+    let result = '';
+
+    if (computerMove === 'paper') {
+        result = 'You lose! computer choose paper and you choose rock.';
+    } else if (computerMove === 'scissors') {
+        result = 'You win! computer choose scissors and you choose rock.';
+    }
+    else {
+        result = 'It\'s a tie! computer choose rock and you choose rock.';
+    }
+    resultEl.innerHTML = result;
+}
+
+function paper() {
+    let computerMove = getcomputerMove();
+    let resultEl = document.getElementById('result');
+    let result = '';
+
+    if (computerMove === 'rock') {
+        result = 'You win! computer choose rock and you choose paper.';
+    } else if (computerMove ==='scissors') {
+        result = 'You lose! computer choose scissors and you choose paper.';
+    }
+    else {
+        result = 'It\'s a tie! computer choose paper and you choose paper.';
+    }
+    resultEl.innerHTML = result;
+}
+
+function scissors() {
+    let computerMove = getcomputerMove();
+    let resultEl = document.getElementById('result');
+    let result = '';
+
+    if (computerMove === 'rock') {
+        result = 'You lose! computer choose rock and you choose scissors.';
+    } else if (computerMove === 'paper') {
+        result = 'You win! computer choose paper and you choose scissors.';
+    }
+    else {
+        result = 'It\'s a tie! computer choose scissors and you choose scissors.';
+    }
+    resultEl.innerHTML = result;
+}
