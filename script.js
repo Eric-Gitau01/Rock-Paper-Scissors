f79e9d2d9a01f624185a9da36c5c1d2f0c55ad43
Eric-Gitau01 <gitaueric01@gmail.com>
2025-05-14 14:29:26 +0300

    structured styled and added functionality for rock paper scissor project

--- script.js
+++ script.js
@@ -1,123 +1,75 @@
-let score = JSON.parse(localStorage.getItem('score')) ||  {
-    wins: 0,
+let rockBtn = document.getElementById("rock-btn");
+let paperBtn = document.getElementById("paper-btn");
+let scissorBtn = document.getElementById("scissor-btn");
+let resultEl = document.getElementById("result-el");
+let scoreEl = document.getElementById("score-el");
+let resetBtn = document.getElementById("reset-btn");
+
+console.log(resetBtn)
+
+let score = JSON.parse(localStorage.getItem("score")) || {
+    win: 0,
     losses: 0,
-    ties: 0
-}
-updateScoreEl()
+    tie: 0
+};
 
-function pickComputerMove() {
-    const randomNumber = Math.random();
+scoreEl.textContent = `win: ${score.win}, losses: ${score.losses}, Ties: ${score.tie}`;
+
+rockBtn.addEventListener("click", function () {
+    playGame("rock");
+});
+paperBtn.addEventListener("click", function () {
+    playGame("paper");
+});
+scissorBtn.addEventListener("click", function () {
+    playGame("scissor");
+});
+
+resetBtn.addEventListener("click", function() {
+    score = {
+         win: 0,
+        losses: 0,
+        tie: 0
+    }
+    scoreEl.textContent = `win: ${score.win}, losses: ${score.losses}, Ties: ${score.tie}`;
+    localStorage.removeItem("score")
+    resultEl.textContent = "Score has been reset and removed from storage.";
+})
+
+function playGame(playermove) {
     let computerMove = '';
-    let result = '';
+    const randomMove = Math.random();
 
-    if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    if (randomMove < 1 / 3) {
         computerMove = 'rock';
-    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    } else if (randomMove < 2 / 3) {
         computerMove = 'paper';
     } else {
-        computerMove = 'scissors';
+        computerMove = 'scissor';
     }
 
-    return computerMove;
+    let result = '';
 
-}
-
-function playGame(playerMove) {
-    const computerMove = pickComputerMove()
-
-     if (playerMove === 'rock') {
-        if (computerMove === 'rock') {
-            result = 'its is a tie';
-            } else if (computerMove === 'paper') {
-                result = 'you lose';
-            } else if (computerMove === 'scissors') {
-                result = 'you win';
-            }
-     } else if (playerMove === 'paper') {
-        if (computerMove === 'rock') {
+    if (playermove === computerMove) {
+        result = 'It\'s a tie';
+        score.tie++
+    } else if (
+        (playermove === 'rock' && computerMove === 'scissor') ||
+        (playermove === 'paper' && computerMove === 'rock') ||
+        (playermove === 'scissor' && computerMove === 'paper')
+    ) {
         result = 'you win';
-        } else if (computerMove === 'paper') {
-            result = 'its is a tie';
-        } else if (computerMove === 'scissors') {
-            result = 'you lose';
-        }
-     } else if (playerMove === 'scissiors') {
-        if (computerMove === 'rock') {
-            result = 'you lose';
-            } else if (computerMove === 'paper') {
-                result = 'you win';
-            } else if (computerMove === 'scissiors') {
-                result = 'its is a tie';
-            }
-     }
-
-     if (result === 'you win') {
-        score.wins += 1
-     } else if (result === 'you lose') {
-        score.losses += 1
-     } else if (result === 'its is a tie') {
-        score.ties +=1
-     }
-
-     updateScoreEl()
-
-     localStorage.setItem('score', JSON.stringify(score))
- 
-    document.querySelector('.js-result').innerHTML = ` ${result}`
-    document.querySelector('.js-moves').innerHTML = `You picked ${playerMove}. Computer picked ${computerMove}.`
-    
-}
-
-function updateScoreEl() {
-    document.querySelector('.js-score').innerHTML = `wins: ${score.wins}, losses: ${score.losses}, ties: ${score.ties}`;
-}
-
-function rock() {
-   playGame('rock')
-}
-
-
-function paper() {
-    playGame('paper')
- 
-}
-
-function scissors() {
-    playGame('scissiors')
-}
-
-function resetScore() {
-    score.wins = 0;
-    score.losses = 0;
-    score.ties = 0;
-    localStorage.removeItem('score')
-    updateScoreEl()
-}
-
-
-function subscribeBtn() {
-    const buttonEl = document.querySelector('.js-subscribe-button')
-
-    if (buttonEl.innerText === 'subscribe') {
-        buttonEl.innerText = 'subscribed'
+        score.win++;
     } else {
-        buttonEl.innerText = 'subscribe'
+        result = 'you lose';
+        score.losses++;
     }
+
+    localStorage.setItem("score", JSON.stringify(score));
+
+    resultEl.innerHTML = `You <img src="images/${playermove}-emoji.png" class="move-icon">
+    <img src="images/${computerMove}-emoji.png" class="move-icon"> computer`;
+
+    scoreEl.textContent = `win: ${score.win}, losses: ${score.losses}, Ties: ${score.tie}`;
 }
 
-function calculateTotal() {
-    const inputEl = document.querySelector('.js-cost-input')
-   let cost = Number(inputEl.value) 
-
-   if (cost < 40) {
-    cost = cost + 10;
-   }
-
-   document.querySelector('.js-total-cost').innerHTML = `$${cost}`
-}
-
-function handleCostKeyDown(event) {
-    if (event.key === 'Enter') {
-        calculateTotal()
-    }
-}
